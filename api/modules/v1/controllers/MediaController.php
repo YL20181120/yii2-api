d915c85754bc0e39a6e804d2925a518d6b56b14f
KillMeAgain <youjingqiang@vip.qq.com>
2015-09-02 18:18:57 +0800

    文件上传 Signed-off-by: KillMeAgain <youjingqiang@vip.qq.com>

--- api/modules/v1/controllers/MediaController.php
+++ api/modules/v1/controllers/MediaController.php
@@ -1,19 +1,59 @@
 <?php
 namespace api\modules\v1\controllers;
 use Yii;
-use api\modules\v1\models\Test;
+use yii\web\UploadedFile;
+use api\modules\v1\exceptions\ErrorException;
+use api\modules\v1\models\Media;
 class MediaController extends Controller
 {
 	public $path;
-	
+	private $form_params = "media";
 	public function init(){
-		$this->path = getcwd();
+		$this->path = getcwd().'/../uploads/';
 	}
-	public function actionIndex() {
-		$request = Yii::$app->request;
-		return $request->getQueryParams();
+	public function actionView($id) {
+		$media = Media::findOne($id);
+		if($media){
+			//Yii::$app->response->sendFile($this->path . $media->newname);
+			Yii::$app->response->sendContentAsFile("sdf", "media");
+		} else {
+			throw new ErrorException("{$id} file not exist!");
+		}
 	}
 	public function actionCreate() {
-		return $this->path;
+		$file = UploadedFile::getInstanceByName($this->form_params);
+		
+		if(!$file){
+			throw new ErrorException("media file can't null",400);
+		}
+		if("jpg" != $file->extension){
+			throw new ErrorException("media extension must be jpg",400);
+		}
+		
+		$uuid = uniqid();
+		$filename = "IMG_" . $uuid .".".$file->extension;
+		try{
+			$file->saveAs($this->path . $filename);
+		} catch (\Exception $e) {
+			throw new ErrorException("The path can't writen",500);
+		}
+		try{
+			$media = new Media();
+			$media->id = $uuid;
+			$media->name = urldecode($file->name);
+			$media->type = $file->extension;
+			$media->size = $file->size;
+			$media->newname = $filename;
+			$media->user    = Yii::$app->user->identity->username;
+			$media->save();
+		} catch (\Exception $e) {
+			throw new ErrorException("Write db error!",500);
+		}
+		
+		return [
+				'code' => 200,
+				'message' => "ok",
+				'media_id'     => $uuid
+		];
 	}
 }
--- api/modules/v1/exceptions/ErrorException.php
+++ api/modules/v1/exceptions/ErrorException.php
@@ -1,1 +1,15 @@
 <?php
+namespace api\modules\v1\exceptions;
+
+use Yii;
+use yii\web\HttpException;
+
+class ErrorException extends \yii\web\HttpException
+{
+	public function __construct($message,$code = 0,$status = 200,\Exception $previous = null){
+		parent::__construct($status,$message,$code,$previous);
+	}
+	public function getName(){
+		return "Api Error";
+	}
+}
